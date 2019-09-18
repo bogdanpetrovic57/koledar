9109ff645d28d7265ba6f29a04ea1537fe0abb0e
Bogdan Petrovic <bogdan037@gmail.com>
2019-09-19 01:44:51 +0200

    3

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -10,71 +10,89 @@ import { HttpModule, Http, ResponseType } from '@angular/http';
 })
 export class CalendarComponent implements OnInit {
 
+/*Spodaj je en enostavni program koledarja. Kot je vidno, izdelan je z uporabo Angular tehnologije. Za izboljšavo HTML 
+  kode sem se poslužil z Bootstrap-om. Obstajata dva načina vnosa in izbire mesecev. 
+  Uporabnik lahko preko combo-boxa izbere določeni mesec in v polje vpiše leto, drugi način je pa da v polje vnese 
+  celoten datum. Ko ni uporabnikovega vnosa, koledar je skrit in se pokaže, ko je gumb "Potrdi" aktiven.
+  
+  Za določanje dnevov sem naredil en model "Dan", ki v sebi hrani podatke :
+    -število danVMesecu
+    -število leto
+    -string mesec
+    -boolean aliJePraznik
+    -boolean aliJeNedelja
+    -boolean aliJePrejsnjiMesec
+  */
 
 
-  leto : number;
-  mesec : string;
-  danZacetka : string;
-  steviloDniVMesecu:number;
-  meseci:string[] = ['januar', 'februar', 'mart', 'april', 'maj', 
+
+  leto: number;
+  mesec: string;
+  steviloDniVMesecu: number;
+  meseci: string[] = ['januar', 'februar', 'mart', 'april', 'maj', 
   'junij', 'julij', 'avgust', 'september', 'oktober', 'november', 'december'];
-  dneviVTednu : string[] = ['Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota', 'Nedelja'];
-  danVTednu :number;
+  dneviVTednu: string[] = ['Ponedeljek', 'Torek', 'Sreda', 'Četrtek', 'Petek', 'Sobota', 'Nedelja'];
+  danVTednu: number;
   mesecStevilka : number;
-  dnevi : Dan[];
-  jeIzbrano : Boolean;
-  reader:FileReader;
+  dnevi: Dan[];
+  jeIzbrano: Boolean;
+  
+  prazniki: string;
+  izbraniDan: number;
 
-
-  prazniki:string;
-  izbraniDan : number;
-
-  text:any;
+  text: any;
 
 
 
 
-  datumIzbira:string;
+  datumIzbira: string;
   
 
-  constructor(private http:Http) { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
-
+    /* Ta delček kode iz "assets" direktorija pridobi tekstualno datoteko, prebere je, in podatke shrani
+      v spremenljivko "this.prazniki". Izvaja se ob začetku programa.*/
     this.http.get('./assets/text.txt').subscribe(data => {
       this.text=((<any>data)._body);
       this.prazniki = this.text.split('\n');
     });
+    /* Omogoča skritost koledarja. Ob pritisku na gumb "Potrdi", se spremenljivka "jeIzbrano" spremeni v true
+      in se koledar prikaže */
     this.jeIzbrano  = false;
     this.dnevi = [];
-    
   }
 
 
 
 
-
+  /*Izberi() je glavna funkcija v programu, in je njena vloga da uporabnikov unos prebere, obdela, izračuna potrebne
+   podatke za prikaz koledarja in omogoči prikaz. */
   izberi(){
     this.dnevi = [];
-
+    /*Izračunamo število dni določenega meseca v določenemu letu*/
     if(this.mesec == '1' || this.mesec == '3' || this.mesec == '5' 
         || this.mesec == '7' || this.mesec == '8' || this.mesec == '10' || this.mesec =='12'){
       this.steviloDniVMesecu = 31;
 
     }else
+    /*Vsako četrto leto je prestopno, s tem da je vsako 100-to leto prestopno samo v primeru če je deljivo z 400.
+      Prestopnost leta se seveda odrazi samo v februarju.*/
     if(this.mesec === '2'){
       if((this.leto % 4 === 0) && (this.leto % 100 !== 0) || (this.leto % 400 === 0)){
         this.steviloDniVMesecu = 29;
       }else{
         this.steviloDniVMesecu = 28;
       }
+    
     }else{
       this.steviloDniVMesecu = 30;
     }
-
+    /*Spodaj iteriramo skozi dneve in delamo objekte tipa Dan. Za vsak objekt je znano leto, mesec in dan v mesecu.
+      S temi podatki lahko preverimo ali je leto nedelja ali praznik. Če je, potem to označimo v booleanu "aliJeNedelja",
+        oziroma "aliJePraznik*/
     this.mesecStevilka = +this.mesec;
     for(var i = 1; i <= this.steviloDniVMesecu; i++){
-     
       let dan = {} as Dan;
       dan.danVMesecu = i;
       dan.leto = this.leto;
@@ -90,8 +108,11 @@ export class CalendarComponent implements OnInit {
        
         dan.aliJePraznik = true;
       }
+      /*Dodamo v seznam*/
       this.dnevi.push(dan);
     }
+
+    /*Spodaj z uporabo tipa Date lahko dobimo dan v tednu določenega datuma. */
     var datum = new Date;
   
     datum.setDate(1);
@@ -99,6 +120,9 @@ export class CalendarComponent implements OnInit {
     datum.setFullYear(this.leto);
     this.danVTednu = datum.getDay();
 
+    /* Ko nam se zgodi, da se mesec ne začne točno v nedeljo, potem do 1. v izbranemu mesecu moramo dodati
+      nekaj dni iz prejšnjega meseca. To izračunamo kot this.mesec - 1 in v slučaju da smo izbrali januar
+      this.mesec je 12, zmanjša se pa leto za 1.*/
     var prejsnjiMesec;
     var leto = this.leto;
     if(this.mesecStevilka == 1){
@@ -107,7 +131,11 @@ export class CalendarComponent implements OnInit {
     }else{
       prejsnjiMesec = this.mesecStevilka-1;
     }
-    
+
+
+    /* Dneve iz prejšnjega meseca bomo dodali na začetek seznama in njihovo število bo ustrezalo
+      potrebnemu številu dni za dopolnitev tabele do začetka trenutno izbranega meseca. Kličemo funkcijo 
+      izracunZadnjihDnevPrejsnjegaMeseca in jih dobimo kot seznam objektov tipa Dan[]. */
     var dneviPrejsnjegaMeseca : Dan[] = [];
     dneviPrejsnjegaMeseca = this.izracunZadnjihDnevPrejsnjegaMeseca(prejsnjiMesec, this.danVTednu, leto);
     for(var i = 0; i < this.danVTednu; i++){
@@ -118,6 +146,9 @@ export class CalendarComponent implements OnInit {
 
 
   izracunZadnjihDnevPrejsnjegaMeseca(prejsnjiMesec:number, steviloZadnjihDnevi:number, leto:number) : Dan[]{
+
+    /*Postopek je podoben kot pri glavni funkciji. Razlika je v tem,
+      da vrnemo samo število potrebnih dni na koncu meseca kot seznam.*/
     var dnevi = [];
     var steviloDnevi = 0;
     if(prejsnjiMesec == 1 || prejsnjiMesec == 3 || prejsnjiMesec == 5 
@@ -140,7 +171,6 @@ export class CalendarComponent implements OnInit {
       dan.aliJeNedelja=false;
       dan.mesec=""+prejsnjiMesec;
       dan.danVMesecu = steviloDnevi - (steviloZadnjihDnevi - i);
-     
       if(this.preveriAliJeNedelja(dan)){
         dan.aliJeNedelja = true;
       }
@@ -150,6 +180,8 @@ export class CalendarComponent implements OnInit {
     return dnevi;
   }
 
+  /* Funkcija ki se kliče ob vnosu v polje datuma. Iz string-a ki ga dobimo iz vmesnika z pomočjo funkcije split
+    lahko izberemo potrebne podatke, in pokličemo funkcijo izberi(), ki bo podatke dalj obdelala.*/
   izberiDatum(){
     var podatki = this.datumIzbira.split('.');
     this.mesec = 0+podatki[1];
@@ -158,9 +190,10 @@ export class CalendarComponent implements OnInit {
 
     this.izberi();
   }
-
+  /* Na začetku imamo pripravljen seznam stringov "prazniki". Iteriramo skozi seznam in preverjamo ali je 
+    praznik ponavljajoč, kar je označeno z "*" na koncu string-a. */
   preveriAliJePraznik(dan : Dan) : boolean {
-  
+
     for(var i = 0; i < this.prazniki.length; i++){
        var day:number= +this.prazniki[i].split('.')[0];
        var month:number = +this.prazniki[i].split('.')[1];
@@ -169,97 +202,40 @@ export class CalendarComponent implements OnInit {
        if(this.prazniki[i].split('.').length == 4){
          ponavljajoci = true;
        }
-
+       
        if(ponavljajoci == false){
+         /* Praznik ni ponavljajoč. Da bi praznik lahko označili v koledarju, moramo preveriti ali 
+          dan, mesec in leto ustrezajo podatkom iz seznama "prazniki".*/
          if(dan.danVMesecu == day && +dan.mesec == month && dan.leto == year){
             return true;
          }
        }
+
        if(ponavljajoci == true){
-         if(i == 0){
-         
-         }
-       
+         /*Če praznik je ponavljajoč, potem nam ni potrebno preverjati ali je leto izbranega meseca enako kot pri 
+          podatku. */
          if(dan.danVMesecu == day && +dan.mesec == month){
-          
            return true;
          }
        }
-       
-
     }
     return false;
   }
 
-    /*izberiDatum(){
-    var podatki = this.datumIzbira.split('.');
-    this.mesec = 0+podatki[1];
-    
-    this.leto = +podatki[2];
-    this.mesecStevilka = +this.mesec;
-
-
-    this.dnevi = [];
-    if(this.mesec == '1' || this.mesec == '3' || this.mesec == '5' 
-        || this.mesec == '7' || this.mesec == '8' || this.mesec == '10' || this.mesec =='12'){
-      this.steviloDniVMesecu = 31;
-    }else
-    if(this.mesec === '2'){
-      if((this.leto % 4 === 0) && (this.leto % 100 !== 0) || (this.leto % 400 === 0)){
-        this.steviloDniVMesecu = 29;
-      }else{
-        this.steviloDniVMesecu = 28;
-      }
-    }else{
-      this.steviloDniVMesecu = 30;
-    }
-    for(var i = 1; i <= this.steviloDniVMesecu; i++){
-      let dan = {} as Dan;
-      dan.danVMesecu = i;
-      dan.leto = this.leto;
-      dan.mesec = this.mesec;
-      this.dnevi.push(dan);
-    }
-    var datum = new Date;
-  
-    datum.setDate(1);
-    datum.setMonth(this.mesecStevilka-1);
-    datum.setFullYear(this.leto);
-    this.danVTednu = datum.getDay();
-
-    var prejsnjiMesec;
-    var leto = this.leto;
-    if(this.mesecStevilka == 1){
-      prejsnjiMesec = 12;
-      leto = this.leto-1;
-    }else{
-      prejsnjiMesec = this.mesecStevilka-1;
-    }
-  
-    var dneviPrejsnjegaMeseca : Dan[] = [];
-    dneviPrejsnjegaMeseca = this.izracunZadnjihDnevPrejsnjegaMeseca(prejsnjiMesec, this.danVTednu, leto);
-    for(var i = 0; i < this.danVTednu; i++){
-      this.dnevi.unshift(dneviPrejsnjegaMeseca.pop());
-    }
-
-
-
-    this.jeIzbrano = true;
-  }*/
-
-  preveriAliJeNedelja(day:Dan){
+  preveriAliJeNedelja(day: Dan){
 
 
     var datum = new Date;
     datum.setDate(day.danVMesecu);
-    datum.setMonth(+day.mesec-1);
-    datum.setFullYear(day.leto, +day.mesec-1, day.danVMesecu);
+    datum.setMonth(+day.mesec - 1);
+    datum.setFullYear(day.leto, +day.mesec - 1, day.danVMesecu);
 
   
-
+    /* Pri tipu Date, če nam metoda getDay() vrne 0, pomeni da je datum nedelja. To v funkciji ki kliče 
+      preveriAliJeNedelja(day: Dan) ustrezno označimo v objektu.*/
     if(datum.getDay() == 0){
       return true;
-    } 
+    }
 
     return false;
   }
