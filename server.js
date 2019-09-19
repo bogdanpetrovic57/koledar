7a3b8f0d478729968d99f3fc596edbc3b4c4ae33
Bogdan Petrovic <bogdan037@gmail.com>
2019-09-19 13:25:30 +0200

    2

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const path = require('path');
+var express    = require('express');
+var app        = express();
 
-const app = express;
 
 app.use(express.static(__dirname + '/dist'));
 
@@ -10,4 +9,6 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
+
+
 
